b6aba8bf3c585346213e2b5fca27defe17014560
apor76-cmyk <apor76@gmail.com>
2026-02-20 03:04:21 +0900

    Add files via upload

---
+++ githubsync.js
@@ -0,0 +1,46 @@
+const github = {
+  user: "apor76-cmyk",
+  repo: "memory_dark",
+  path: "memory_sync.json",
+  token: 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"
+};
+
+// 데이터 불러오기
+async function loadCloud(){
+  const url = `https://api.github.com/repos/${github.user}/${github.repo}/contents/${github.path}`;
+
+  const res = await fetch(url,{
+    headers:{ Authorization:`token ${github.token}` }
+  });
+
+  if(!res.ok) return {};
+
+  const data = await res.json();
+  const content = decodeURIComponent(escape(atob(data.content)));
+  return JSON.parse(content);
+}
+
+// 데이터 저장
+async function saveCloud(state){
+  const url = `https://api.github.com/repos/${github.user}/${github.repo}/contents/${github.path}`;
+
+  let sha=null;
+  const res = await fetch(url,{ headers:{ Authorization:`token ${github.token}` }});
+  if(res.ok){
+    const data = await res.json();
+    sha = data.sha;
+  }
+
+  await fetch(url,{
+    method:"PUT",
+    headers:{
+      "Content-Type":"application/json",
+      Authorization:`token ${github.token}`
+    },
+    body:JSON.stringify({
+      message:"update data",
+      content:btoa(unescape(encodeURIComponent(JSON.stringify(state)))),
+      sha
+    })
+  });
+}
